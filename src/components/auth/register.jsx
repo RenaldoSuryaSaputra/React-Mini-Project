679df6e7524fcd7955adc3f8c771a7726de0b479
Renaldo <renaldo.surya@ti.ukdw.ac.id>
2023-05-12 17:33:04 +0700

    commit: revisi

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -135,7 +135,7 @@ const Register = () => {
                       })}
                       className="w-80 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-2"
                       type="text"
-                      placeholder="81295041613"
+                      placeholder="example"
                     />
                   </div>
                   {errors.contact && (
